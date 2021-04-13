2f3760ea4109a88ad7bd3f56318ad2f5c4a17d14
joey101 <jawad.tanana@gmail.com>
2021-04-13 10:12:02 +1000

    part d

--- lab8/part_d.py
+++ lab8/part_d.py
@@ -4,4 +4,16 @@ Created on Tue Apr 13 10:00:38 2021
 
 @author: Jawad
 """
+import numpy as np
+import sim_projectile as sim
+import math as m
 
+
+angle_start = 20
+angle_final = 60
+increment = 1
+launch_array = np.arange(angle_start, angle_final + increment, increment)
+
+
+
+print(launch_array);
