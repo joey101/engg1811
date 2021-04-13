cbf1dda0c90012bf99a02a7b8cba4ab93988e459
joey101 <jawad.tanana@gmail.com>
2021-04-13 19:16:03 +1000

    labs

---
+++ lab9/distance.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ENGG1811 Lab
+
+Exercise on numpy vectorisation 
+"""
+import numpy as np
+import math as m
+
+pos = np.array([[   1.72,   2.56],
+                [   0.24,   5.67],
+                [  -1.24,   5.45],
+                [  -3.17,  -0.23],
+                [   1.17,  -1.23],
+                [   1.12,   1.08]])
+
+ref = np.array([1.22, 1.18])
+
+
+
+
+
+
+# The expected answer is an array with 6 elements. 
+# The elements are approximately:
+# [ 1.468,  4.596,  4.928 ,  4.611,  2.410,  0.141 ]
+
+
+# Insert your solution below
+
