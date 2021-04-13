2943d7525d809e9b010d673c531f58860acae8e3
joey101 <jawad.tanana@gmail.com>
2021-04-13 19:15:11 +1000

    idk

--- lab8/part_d.py
+++ lab8/part_d.py
@@ -7,13 +7,36 @@ Created on Tue Apr 13 10:00:38 2021
 import numpy as np
 import sim_projectile as sim
 import math as m
-
+import matplotlib.pyplot as plt
 
 angle_start = 20
 angle_final = 60
 increment = 1
-launch_array = np.arange(angle_start, angle_final + increment, increment)
+landing_height = 10
+mass = 0.145
+c = 0.0013
+v0 = 50
+landing_level = 10
 
 
+launch_angle = np.arange(angle_start, angle_final + increment, increment)
 
-print(launch_array);
+time_array = np.arange(0,10,0.1)
+landing_times = np.zeros_like(launch_angle, dtype = 'float')
+landing_distance = np.zeros_like(launch_angle, dtype = 'float')
+
+for angle in launch_angle:
+    position_x,position_y = sim.sim_projectile(time_array,mass,c,v0,angle)
+    pos, time = sim.find_landing_pos_time(time_array,position_x,position_y,landing_level)
+    landing_distance = np.append(landing_distance, pos)
+    landing_times = np.append(landing_times, time)
+
+    
+plt.figure(1)
+plt.plot(landing_times, landing_distance)
+plt.grid()
+plt.xlabel('Landing Time')
+plt.ylabel('Landing Position')
+plt.title('Position of the strawberry')
+plt.show()
+
