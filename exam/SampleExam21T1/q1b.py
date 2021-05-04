ca6d5d8e7f188215de49a85b817ed5ed0f2208b3
joey101 <jawad.tanana@gmail.com>
2021-05-04 17:14:35 +1000

    practice exam

--- exam/SampleExam21T1/q1b.py
+++ exam/SampleExam21T1/q1b.py
@@ -6,6 +6,21 @@ ENGG1811 18s2 exam
 Template for Question 1b. 
 """
 
+
+import numpy as np
+
+
 def q1b_func(a_list,m):
-   
-        
+    
+    tmp = np.zeros(len(a_list))
+    
+    for i in range(len(a_list)):
+        boo = i < m
+        print(boo)
+        if boo == True:
+            tmp = a_list.append(a_list.index(a_list[i]))
+        print(tmp)
+              
+            
+            
+    return tmp 
