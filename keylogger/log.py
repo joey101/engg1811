fa0987c47e915988f9074e662393e386c6a278c4
joey101 <jawad.tanana@gmail.com>
2021-05-22 20:01:05 +1000

    opening file

--- keylogger/log.py
+++ keylogger/log.py
@@ -1,1 +1,18 @@
+"""
+    Creating a key logging program.
+
+    The goal is to log key strokes with the date and time for each sentence.
+
+"""
+
+
 import numpy as np
+import time as t
+
+
+with open("/home/bladerunner/Documents/python/keylogger/log.txt",'r') as logging:
+    for line in logging: 
+        print("first line contains: ", line)
+
+
+
