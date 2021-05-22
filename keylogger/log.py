9ecf1da23c6ed6fd877b19d0c3060d71eadb5ce6
joey101 <jawad.tanana@gmail.com>
2021-05-22 22:39:47 +1000

    adding date and time to the file

--- keylogger/log.py
+++ keylogger/log.py
@@ -7,12 +7,25 @@
 
 
 import numpy as np
-import time as t
+from datetime import datetime as t
 
 
-with open("/home/bladerunner/Documents/python/keylogger/log.txt",'r') as logging:
-    for line in logging: 
-        print("first line contains: ", line)
+
+
+
+
+with open("/home/bladerunner/Documents/python/keylogger/log.txt",'a') as logging:
+    # This gets the date and time in 
+    # dd/mm/yyyy and HH:MM:SS
+    now = t.now()
+    current_dt = now.strftime("[%d/%m/%Y %H:%M:%S]: ")
+    # This only gets the time HH:MM:SS
+    time_only = t.now()
+    current_t = now.strftime("[%H:%M:%S]: ")
+    
+    # This appends to a file.
+    logging.write(current_t)
+    logging.write("First line contains")
 
 
 
