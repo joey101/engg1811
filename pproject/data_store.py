8c98bd951a74e63888c02666ddaa7b9cddd3b134
joey101 <jawad.tanana@gmail.com>
2022-03-02 19:25:29 +1100

    testing

---
+++ pproject/data_store.py
@@ -0,0 +1,40 @@
+data = {
+    'users': [
+        {
+            'id': 1,
+            'name' : 'user1',
+        },
+        {
+            'id': 2,
+            'name' : 'user2',
+        },
+    ],
+    'channels': [
+        {
+            'id': 1,
+            'name' : 'channel1',
+        },
+        {
+            'id': 2,
+            'name' : 'channel2',
+        },
+    ],
+}
+
+## YOU ARE ALLOWED TO CHANGE THE BELOW IF YOU WISH
+class Datastore:
+    def __init__(self):
+        self.__store = initial_object
+
+    def get(self):
+        return self.__store
+
+    def set(self, store):
+        if not isinstance(store, dict):
+            raise TypeError('store must be of type dictionary')
+        self.__store = store
+
+print('Loading Datastore...')
+
+global data_store
+data_store = Datastore()
---
+++ pproject/test.py
@@ -0,0 +1,9 @@
+import data_store
+
+num = 0
+idk = data_store.get()
+
+
+data['users'][num]['id'] = 5
+store = data['users'][num]['id']
+print(store)
