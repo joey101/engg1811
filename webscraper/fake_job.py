6767462befe0d2de94217b00583fa32c623db41f
joey101 <jawad.tanana@gmail.com>
2022-08-29 11:44:53 +1000

    webscraper for fake website

--- webscraper/fake_job.py
+++ webscraper/fake_job.py
@@ -7,21 +7,27 @@ import requests
 from bs4 import BeautifulSoup
 
 # The URL to be scraped.
-URL = "https://realpython.github.io/fake-jobs/"
+URL = "https://pythonjobs.github.io/"
 
-# Requests data from the "server" i.e the URL
+#Requests data from the "server" i.e the URL
 page = requests.get(URL)
 # Testing 
 #print(page.text)
 
 # Takes html content as input. (used .content instead of .text in order to get
 # raw bytes)
+
 soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find(id="ResultsContainer")
+results = soup.find("section", class_="job_list")
+#print(results.text)
 
-job_elements = results.find_all("div", class_="card-content")
-
+job_elements = results.find_all("div", class_="job")
+# print(job_elements)
+count = 1
 for i in job_elements:
-    job_title = i.find("h2", class_="title")
-    print(job_title.text.strip())
+    # Finds the HTML class/elemnt that we want to read, then strips it to just 
+    # the words on the screen. h2 = the type of title
+    job_title = i.find("h1")
     
+    print(str(count)+ ".", job_title.text.strip())
+    count += 1
